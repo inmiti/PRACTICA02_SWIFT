a93d7c6906178bf88156a832ffc8c2b35e27aae2
inmiti <inmabauvel@gmail.com>
2023-07-09 13:57:19 +0200

    Creo clase HotelReservationManger con funcion reservationAdd. Pendiente de probar

--- HRManager.swift
+++ HRManager.swift
@@ -7,10 +7,39 @@
 
 import Foundation
 
-struct Client
+struct Client {
+    let name: String
+    let age: Int
+    let heigh: Double
+}
 
+struct Reservation {
+    let id: Int
+    let hotelName: String
+    let clients: []
+    let days: Int
+    let price: Double
+    let breakfast: Bool
+}
 
-struct Reservation
+enum ReservationError {
+    case id
+    case reserved
+    case notReserved
+}
 
+class HotelReservationManager {
+    var reservationList: [Reservation] = []
+    
+    func reservationAdd(hotelName: String,clientslist: [Client], daysInHotel: Int, price: Double, breakfast: Bool) -> Reservation {
+        
+        let newId = (reservationList.last?.id ?? 0 ) + 1
+        
+        let newReservation = Reservation(id: newId, hotelName: hotelName, clientslist: clientslist, daysInHotel: daysInHotel, price: price, breakfast: breakfast)
+        reservationList.append(newReservation)
+        
+        return newReservation
+    }
+}
 
-struct ReservationError
+    
